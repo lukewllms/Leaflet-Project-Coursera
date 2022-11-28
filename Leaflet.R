6891a83fc59e5ecd3ff2f5753cd353aeddc607f3
Luke <109493426+lukewllms@users.noreply.github.com>
2022-11-27 21:40:29 -0500

    Delete Leaflet.R

--- Leaflet.R
+++
@@ -1,8 +0,0 @@
-library(leaflet)
-set.seed(1)
-df <- data.frame(lat= runif(30, min = 25, max = 31), long=runif(30, min = 80, max = 86))
-head(df)
-df %>%
-  leaflet( width = 900) %>%
-  addTiles() %>%
-  addMarkers(clusterOptions = markerClusterOptions(), popup = "Hi")
